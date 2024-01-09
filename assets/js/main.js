ab9dc932c280c602fa4560d8c6da7b47c373e6ef
rasih-ozturk <rasih@techmeanlab.com>
2024-01-10 01:08:48 +0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,24 +36,76 @@ var swiper = new Swiper(".mySwiper", {
   watchSlidesProgress: true,
   spaceBetween: 30,
   centeredSlides: true,
-  speed: 1000, 
+  speed: 1000,
   loop: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
 });
-
 
 var swiper = new Swiper(".mySwiperTwo", {
   slidesPerView: 4,
   watchSlidesProgress: true,
   spaceBetween: 30,
-  speed: 1000, 
+  speed: 1000,
   loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
 });
+function slideMenu() {
+  var headerInside = document.querySelector(".headerinside");
+  headerInside.classList.toggle("headerinsideActive");
+  $(".headerinside ul li a").fadeToggle(3000);
+}
 
+$(function () {
+  $("#menu-wrapper").click(function (event) {
+    event.stopPropagation();
+    $("#hamburger-menu").toggleClass("open");
+    $("#menu-container .menu-list").toggleClass("headerinsideActive");
+    slideMenu();
+
+    $("body").toggleClass("overflow-hidden");
+  });
+
+  $(".headerinside")
+    .find(".headerinside")
+    .click(function () {
+      $(this).next().toggleClass("open").slideToggle("fast");
+      $(this)
+        .toggleClass("headerinsideActive")
+        .find(".menu-link")
+        .toggleClass("headerinsideActive");
+
+      $(".headerinside .accordion-content")
+        .not($(this).next())
+        .slideUp("fast")
+        .removeClass("open");
+      $(".headerinside .accordion-toggle")
+        .not(jQuery(this))
+        .removeClass("headerinsideActive")
+        .find(".headerinside")
+        .removeClass("headerinsideActive");
+    });
+}); // jQuery load
