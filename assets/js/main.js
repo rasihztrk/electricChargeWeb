bf7d83f7c28e37d5283a6ca3ef86b3c79d845e47
rasih-ozturk <rasih@techmeanlab.com>
2024-01-09 22:20:37 +0300

    update

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+$("#1").waypoint(
+  function () {
+    increment(1, 70);
+  },
+  { offset: "75%" }
+);
+
+$("#2").waypoint(
+  function () {
+    increment(2, 81);
+  },
+  { offset: "75%" }
+);
+
+$("#3").waypoint(
+  function () {
+    increment(3, 100);
+  },
+  { offset: "75%" }
+);
+
+function increment(elem, finalVal) {
+  var currVal = parseInt(document.getElementById(elem).innerHTML, 10);
+  if (currVal < finalVal) {
+    currVal++;
+    document.getElementById(elem).innerHTML = currVal;
+
+    setTimeout(function () {
+      increment(elem, finalVal);
+    }, 40);
+  }
+}
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1.6,
+  watchSlidesProgress: true,
+  spaceBetween: 30,
+  centeredSlides: true,
+  speed: 1000, 
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
+var swiper = new Swiper(".mySwiperTwo", {
+  slidesPerView: 4,
+  watchSlidesProgress: true,
+  spaceBetween: 30,
+  speed: 1000, 
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
